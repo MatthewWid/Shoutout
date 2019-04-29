bf70145535b7e307d759b3b635f21adf4cc3fced
MatthewWid <115mattey@gmail.com>
2019-04-29 12:39:48 +1000

    Add Mongoose database connection.

--- server.js
+++ server.js
@@ -3,11 +3,17 @@ require("dotenv").config({
 });
 
 const path = require("path");
+const mongoose = require("mongoose");
 const express = require("express");
 const routes = require("./routes");
-
 const app = express();
-// Connect to database
+
+// Database
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.DATABASE_URL, {
+	useNewUrlParser: true
+});
+const {connection: db} = mongoose;
 
 // View Engine
 app.set("views", path.join(__dirname, "/views"));
@@ -18,7 +24,13 @@ app.use("/", routes);
 
 // Port
 app.set("port", process.env.PORT || 80);
-// Server
-app.listen(app.get("port"), () => {
-	console.log(`Listening on port ${app.get("port")}`);
+
+// Wait for database connection and start server
+db.on("error", (err) => {
+	console.error(`ERROR Database Connection: ${err}`);
+});
+db.on("open", () => {
+	app.listen(app.get("port"), () => {
+		console.log(`SERVER Listening on port ${app.get("port")}.`);
+	});
 });
