cf3b89cdd96079de35996b22f5e8d8373bf7167a
MatthewWid <115mattey@gmail.com>
2019-05-02 23:00:40 +1000

    Update footer copyright icon to be a name code instead.

--- controllers/postController.js
+++ controllers/postController.js
@@ -3,7 +3,6 @@ const Post = require("../models/Post");
 
 exports.homePage = async (req, res) => {
 	const posts = await Post.find();
-	console.log(posts);
 
 	res.render("home", {
 		title: "Homepage",
