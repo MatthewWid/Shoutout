6fcc1b7f490b2fe34feeb1a1ede5dc4d76c9757b
MatthewWid <115mattey@gmail.com>
2019-06-05 23:47:24 +1000

    Add bundle splitting for vendor code. Add test 'home.js' and 'profile.js' files.

--- config/webpack.common.config.js
+++ config/webpack.common.config.js
@@ -7,6 +7,7 @@ const SRCDIR_SCSS = path.resolve(__dirname, "..", "./src/scss/");
 const config = {
 	entry: {
 		"js/home": path.resolve(SRCDIR_JS, "home.js"),
+		"js/profile": path.resolve(SRCDIR_JS, "profile.js"),
 		"css/main": path.resolve(SRCDIR_SCSS, "main.scss")
 	},
 	module: {
@@ -24,6 +25,18 @@ const config = {
 	output: {
 		path: path.resolve(__dirname, "..", "./public/"),
 		filename: "[name].js"
+	},
+	optimization: {
+		splitChunks: {
+			cacheGroups: {
+				vendor: {
+					test: /node_modules/,
+					chunks: "initial",
+					name: "js/vendor",
+					enforce: true
+				}
+			}
+		}
 	},
 	plugins: [
 		new FixStyleOnlyEntirePlugin(),
--- src/js/home.js
+++ src/js/home.js
@@ -1,3 +1,3 @@
 import React from "react";
 
-console.log(react);
+console.log("first import");
