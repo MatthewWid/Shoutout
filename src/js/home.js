3cf5d1f983f254896ea1dfb68b3118ab127fd48e
MatthewWid <115mattey@gmail.com>
2019-06-15 23:25:05 +1000

    Move vendor scripts to base layout template.

--- src/js/home.js
+++ src/js/home.js
@@ -1,4 +1,4 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-console.log("test");
+console.log("test1");
--- src/js/profile.js
+++ src/js/profile.js
@@ -1,3 +1,3 @@
 import React from "react";
 
-console.log("second import");
+console.log("test2");
