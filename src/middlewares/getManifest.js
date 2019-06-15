b9c9511988cb21f7767d641a49237fafd557cc10
MatthewWid <115mattey@gmail.com>
2019-06-16 01:21:11 +1000

    Add graceful error handling to 'getManifest' middleware if the manifest file does not exist.

--- src/middlewares/getManifest.js
+++ src/middlewares/getManifest.js
@@ -3,7 +3,14 @@ const jsonfile = require("jsonfile");
 const MANIFEST_FILE = path.resolve(__dirname, "..", "./webpack-manifest.json");
 
 function getManifest() {
-	const file = jsonfile.readFileSync(MANIFEST_FILE);
+	let file = null;
+
+	try {
+		file = jsonfile.readFileSync(MANIFEST_FILE);
+	} catch (err) {
+		console.error("SERVER No webpack-manifest file found.");
+		process.exitCode = 1;
+	}
 
 	return (req, res, next) => {
 		res.locals.manifest = file;
