e1a352e279e83563a360b1d6cf74c8482ba6c52a
MatthewWid <115mattey@gmail.com>
2019-06-20 18:55:30 +1000

    Add controller for creating a user.

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,11 +1,14 @@
 const mongoose = require("mongoose");
 const User = mongoose.model("User");
 
-exports.register = async (req, res, next) => {
-	// const newUser = new User({
-	// 	name: "Matthew",
-	// 	email: "mianamal12@gmail.com"
-	// });
-	// await User.register(newUser, "hunter2")
-	res.status("200").send("Attempted to register.");
+exports.createUser = async (req, res, next) => {
+	const {username, email, password} = req.body;
+
+	const newUser = new User({
+		name: username,
+		email: email
+	});
+	const registeredUser = await User.register(newUser, password);
+
+	return next();
 };
