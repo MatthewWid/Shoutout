86868d524037665f15c09b4aa4302163105910e7
MatthewWid <115mattey@gmail.com>
2019-07-02 18:19:50 +1000

    Move conditional login rendering to SessionDetails component instead of the Header component.

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -1,33 +1,24 @@
 import React from "react";
 import SessionDetails from "./SessionDetails.js";
 
-class Header extends React.Component {
-	render() {
-		let sessionElement;
-
-		if (this.props.user) {} else {
-			sessionElement = (
-				<SessionDetails
-					login={this.props.login}
-					signup={this.props.signup}
-				/>
-			);
-		}
-
-		return (
-			<div className="header-container">
-				<header className="header">
-					<nav className="header__nav">Navigation</nav>
-					<div className="header__logo">
-						<img src="./images/logo/logo.png" alt="Shoutout" />
-					</div>
-					<div className="header__extra">
-						{sessionElement}
-					</div>
-				</header>
-			</div>
-		);
-	}
-}
+const Header = (props) => {
+	return (
+		<div className="header-container">
+			<header className="header">
+				<nav className="header__nav">Navigation</nav>
+				<div className="header__logo">
+					<img src="./images/logo/logo.png" alt="Shoutout" />
+				</div>
+				<div className="header__extra">
+					<SessionDetails
+						user={props.user}
+						login={props.login}
+						signup={props.signup}
+					/>
+				</div>
+			</header>
+		</div>
+	);
+};
 
 export default Header;
--- src/js/components/SessionDetails.js
+++ src/js/components/SessionDetails.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {Fragment} from "react";
 import UserEntry from "./UserEntry.js";
 
 class SessionDetails extends React.Component {
@@ -15,17 +15,28 @@ class SessionDetails extends React.Component {
 	}
 
 	render() {
+		let details;
+		if (this.props.user) {
+			details = <p>Now logged in</p>;
+		} else {
+			details = (
+				<Fragment>
+					<p className="session__login-link" onClick={this.toggleForm}>
+						Have an account? <b>Log in</b>
+					</p>
+					<div className={`session__dropdown ${this.state.formOpen ? "session__dropdown--open" : ""}`}>
+						<UserEntry
+							login={this.props.login}
+							signup={this.props.signup}
+						/>
+					</div>
+				</Fragment>
+			);
+		}
+
 		return (
 			<div className="session">
-				<p className="session__login-link" onClick={this.toggleForm}>
-					Have an account? <b>Log in</b>
-				</p>
-				<div className={`session__dropdown ${this.state.formOpen ? "session__dropdown--open" : ""}`}>
-					<UserEntry
-						login={this.props.login}
-						signup={this.props.signup}
-					/>
-				</div>
+				{details}
 			</div>
 		);
 	}
