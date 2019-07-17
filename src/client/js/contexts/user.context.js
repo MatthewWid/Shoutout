af696a893f9152d70cef63774e07bc6c396d5216
MatthewWid <115mattey@gmail.com>
2019-07-17 19:01:34 +1000

    Add default no-op 'authUser' function to UserContext.

--- src/client/js/contexts/user.context.js
+++ src/client/js/contexts/user.context.js
@@ -2,7 +2,8 @@ import React from "react";
 
 const UserContext = React.createContext({
 	user: null,
-	setUser: () => {}
+	setUser: () => {},
+	authUser: () => {}
 });
 
 export default UserContext;
