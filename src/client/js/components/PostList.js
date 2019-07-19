d1308cd5d8bf00cbbc9314cded35b695ee9c43c1
MatthewWid <115mattey@gmail.com>
2019-07-19 17:56:53 +1000

    Update PostList component to show a message indicating there are no posts if there are no posts provided.

--- src/client/js/components/PostList.js
+++ src/client/js/components/PostList.js
@@ -3,14 +3,20 @@ import Post from "./Post.js";
 
 const PostList = (props) => (
 	<div className="post-list">
-		{props.posts.map((post, index) => (
-			<Post
-				key={index}
-				post={post}
-				updatePost={props.updatePost}
-				removePost={props.removePost}
-			/>
-		))}
+		{
+			(props.posts && props.posts.length !== 0) &&
+
+			props.posts.map((post, index) => (
+				<Post
+					key={index}
+					post={post}
+					updatePost={props.updatePost}
+					removePost={props.removePost}
+				/>
+			)) ||
+
+			<p className="post-list__empty">There are no posts here, yet.</p>
+		}
 	</div>
 );
 
