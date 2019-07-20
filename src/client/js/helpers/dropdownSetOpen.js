1d0328f2aa0f7edb4fa308627a47405f61145713
MatthewWid <115mattey@gmail.com>
2019-07-20 11:15:03 +1000

    Update Dropdown component to not re-render when clicking outside of the dropdown when the dropdown is already closed avoiding many extra unnecessary re-renders.

--- src/client/js/helpers/dropdownSetOpen.js
+++ src/client/js/helpers/dropdownSetOpen.js
@@ -2,6 +2,12 @@ export default function(change) {
 	const action = change;
 
 	return async () => {
+		// If the state is always the result of the action abort the operation
+		// avoiding an extra unnecessary re-render.
+		if (this.state.dropdownOpen === action) {
+			return;
+		}
+
 		this.setState({
 			dropdownOpen: action
 		});
