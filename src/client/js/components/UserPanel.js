f3dce6124d7b14c31bb127116ba1e5d61af8210b
MatthewWid <115mattey@gmail.com>
2019-07-22 17:59:00 +1000

    Convert UserPanel component to a functional component.

--- src/client/js/components/UserPanel.js
+++ src/client/js/components/UserPanel.js
@@ -2,20 +2,11 @@ import React from "react";
 import {withUserContext} from "../contexts/user.context.js";
 import DashboardProfileCard from "./DashboardProfileCard.js";
 
-class UserPanel extends React.Component {
-	render() {
-		const {user} = this.props.UserContext;
-
-		if (!user) {
-			return null;
-		}
-
-		return (
-			<div className="content__panel card user">
-				<DashboardProfileCard user={user} />
-			</div>
-		);
-	}
-}
+// If there is no user logged in do not render
+const UserPanel = ({UserContext: {user}}) => !user ? null : (
+	<div className="content__panel card user">
+		<DashboardProfileCard user={user} />
+	</div>
+);
 
 export default withUserContext(UserPanel);
