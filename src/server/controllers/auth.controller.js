8ef5493457390d3dfce0b71bb73cbc6b18ea19f2
MatthewWid <115mattey@gmail.com>
2019-07-22 23:08:01 +1000

    Update 'login' controller to return a JSON response if the login attempt is not successful.

--- src/server/controllers/auth.controller.js
+++ src/server/controllers/auth.controller.js
@@ -2,7 +2,23 @@ const passport = require("passport");
 const validator = require("express-validator");
 const valErrMsg = require("../helpers/validationErrorMsg.js");
 
-exports.login = passport.authenticate("local");
+exports.login = (req, res, next) => {
+	passport.authenticate("local", (err, user, info) => {
+		if (err) {
+			return next(err);
+		}
+		if (!user) {
+			return res
+				.status(401)
+				.json({
+					success: false,
+					msg: "Username or password is incorrect."
+				});
+		}
+
+		req.logIn(user, (err) => next(err));
+	})(req, res, next);
+};
 
 exports.logout = (req, res) => {
 	req.logout();
