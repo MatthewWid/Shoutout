cead25f666f4de785f46a487c2187e7fa580eb74
MatthewWid <115mattey@gmail.com>
2019-07-24 20:23:34 +1000

    Update Header to generate a Link component list from an array of link data. Add links for /notifications and /messages. Update Header to enforce a flex amount on its children so that each header section is of equal width.

--- src/client/js/components/Header.js
+++ src/client/js/components/Header.js
@@ -2,22 +2,39 @@ import React from "react";
 import {Link} from "react-router-dom";
 import SessionDetails from "./SessionDetails.js";
 
+const links = [
+	{
+		text: "Home",
+		name: "home",
+		to: "/"
+	},
+	{
+		text: "Notifications",
+		name: "notifications",
+		to: "/notifications"
+	},
+	{
+		text: "Messages",
+		name: "messages",
+		to: "/messages"
+	}
+];
+
 const Header = (props) => {
+	const linkEls = links.map((link) => (
+		<Link className="header__link" to={link.to}>{link.text}</Link>
+	));
+
 	return (
 		<div className="header-container">
 			<header className="header">
-				<nav className="header__nav">
-					<Link
-						className="header__link"
-						to="/"
-					>
-						Home
-					</Link>
+				<nav className="header__section header__nav">
+					{linkEls}
 				</nav>
-				<div className="header__logo">
+				<div className="header__section header__logo">
 					<img src="./images/logo/logo.png" alt="Shoutout" />
 				</div>
-				<div className="header__extra">
+				<div className="header__section header__extra">
 					<SessionDetails
 						login={props.login}
 						signup={props.signup}
