5170a37f16dd5e7b77ffa0ae128a5a72ba87f426
MatthewWid <115mattey@gmail.com>
2019-07-30 11:59:27 +1000

    Add quick links section to PanelSiteInfo with links to project source and /attribution page.

--- src/client/js/components/PanelSiteInfo.js
+++ src/client/js/components/PanelSiteInfo.js
@@ -1,11 +1,24 @@
 import React from "react";
+import {Link} from "react-router-dom";
 import SiteStats from "./SiteStats.js";
 
 const PanelSiteInfo = (props) => {
 	return (
 		<div className="content__panel panel-site-info">
 			<SiteStats />
-			<div className="panel-site-info__copyright card">
+			<div className="panel-site-info__section panel-site-info__links card">
+				<a
+					className="panel-site-info__link"
+					href="https://github.com/MatthewWid/Shoutout"
+					target="_blank"
+				>Source Code</a>
+
+				<Link
+					className="panel-site-info__link"
+					to="/attribution"
+				>Attribution</Link>
+			</div>
+			<div className="panel-site-info__section card">
 				<p>&copy; {(new Date()).getFullYear()} Matthew W.</p>
 			</div>
 		</div>
