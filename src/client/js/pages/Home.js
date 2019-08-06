6068b946d2840f184366929b000e45812d57d137
MatthewWid <115mattey@gmail.com>
2019-08-06 21:56:20 +1000

    Update Home page to show the curated feed when the user is logged in, otherwise show all posts.

--- src/client/js/pages/Home.js
+++ src/client/js/pages/Home.js
@@ -1,10 +1,13 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useContext} from "react";
+import UserContext from "../contexts/user.context.js";
 import Layout from "./Layout.js";
 import PanelUser from "../components/PanelUser.js";
 import PanelFeed from "../components/PanelFeed.js";
 import PanelSiteInfo from "../components/PanelSiteInfo.js";
 
 const Home = (props) => {
+	const {loginStatus} = useContext(UserContext);
+
 	useEffect(() => {
 		document.title = "Shoutout"
 	}, []);
@@ -13,6 +16,7 @@ const Home = (props) => {
 		<Layout page="home">
 			<PanelUser />
 			<PanelFeed
+				param={loginStatus === 2 ? "curated" : null}
 				query={{
 					sort: "new"
 				}}
