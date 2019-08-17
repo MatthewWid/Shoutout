10fe07e180e08c1ffd0ac35a0f017b36300bc430
MatthewWid <115mattey@gmail.com>
2019-08-17 14:58:44 +1000

    Add 'cog', 'sign-out-alt' and 'user' SVGs to attributions page.

--- src/client/js/pages/Attribution.js
+++ src/client/js/pages/Attribution.js
@@ -11,24 +11,15 @@ const Attribution = () => {
 			<div className="content__panel card attribution">
 				<h1 className="attribution__header">Attributions</h1>
 				<ul className="attribution__list">
-					<li>
-						<a href="https://fontawesome.com/icons/heart?style=regular">"heart (Regular Style)"</a> by <a href="https://fontawesome.com/license/free">FontAwesome</a> is licenced under <a href="https://creativecommons.org/licenses/by/4.0/">CC BY 4.0</a>
-					</li>
-					<li>
-						<a href="https://fontawesome.com/icons/heart?style=solid">"heart (Solid Style)"</a> by <a href="https://fontawesome.com/license/free">FontAwesome</a> is licenced under <a href="https://creativecommons.org/licenses/by/4.0/">CC BY 4.0</a>
-					</li>
-					<li>
-						<a href="https://fontawesome.com/icons/moon?style=regular">"moon (Regular Style)"</a> by <a href="https://fontawesome.com/license/free">FontAwesome</a> is licenced under <a href="https://creativecommons.org/licenses/by/4.0/">CC BY 4.0</a>
-					</li>
-					<li>
-						<a href="https://fontawesome.com/icons/moon?style=solid">"moon (Solid Style)"</a> by <a href="https://fontawesome.com/license/free">FontAwesome</a> is licenced under <a href="https://creativecommons.org/licenses/by/4.0/">CC BY 4.0</a>
-					</li>
-					<li>
-						<a href="https://fontawesome.com/icons/ellipsis-v?style=solid">"ellipsis-v (Solid Style)"</a> by <a href="https://fontawesome.com/license/free">FontAwesome</a> is licenced under <a href="https://creativecommons.org/licenses/by/4.0/">CC BY 4.0</a>
-					</li>
-					<li>
-						<a href="https://loading.io/icon/kbmzeh">"comet" spinner</a> by <a href="https://loading.io/">loading.io</a>  is licenced under <a href="https://creativecommons.org/licenses/by/4.0/">CC BY 4.0</a>
-					</li>
+					<li><a href="https://fontawesome.com/icons/heart?style=regular">"heart (Regular Style)"</a> by <a href="https://fontawesome.com/license/free">FontAwesome</a> is licenced under <a href="https://creativecommons.org/licenses/by/4.0/">CC BY 4.0</a></li>
+					<li><a href="https://fontawesome.com/icons/heart?style=solid">"heart (Solid Style)"</a> by <a href="https://fontawesome.com/license/free">FontAwesome</a> is licenced under <a href="https://creativecommons.org/licenses/by/4.0/">CC BY 4.0</a></li>
+					<li><a href="https://fontawesome.com/icons/moon?style=regular">"moon (Regular Style)"</a> by <a href="https://fontawesome.com/license/free">FontAwesome</a> is licenced under <a href="https://creativecommons.org/licenses/by/4.0/">CC BY 4.0</a></li>
+					<li><a href="https://fontawesome.com/icons/moon?style=solid">"moon (Solid Style)"</a> by <a href="https://fontawesome.com/license/free">FontAwesome</a> is licenced under <a href="https://creativecommons.org/licenses/by/4.0/">CC BY 4.0</a></li>
+					<li><a href="https://fontawesome.com/icons/ellipsis-v?style=solid">"ellipsis-v (Solid Style)"</a> by <a href="https://fontawesome.com/license/free">FontAwesome</a> is licenced under <a href="https://creativecommons.org/licenses/by/4.0/">CC BY 4.0</a></li>
+					<li><a href="https://fontawesome.com/icons/cog?style=solid">"cog (Solid Style)"</a> by <a href="https://fontawesome.com/license/free">FontAwesome</a> is licenced under <a href="https://creativecommons.org/licenses/by/4.0/">CC BY 4.0</a></li>
+					<li><a href="https://fontawesome.com/icons/sign-out-alt?style=solid">"sign-out-alt (Solid Style)"</a> by <a href="https://fontawesome.com/license/free">FontAwesome</a> is licenced under <a href="https://creativecommons.org/licenses/by/4.0/">CC BY 4.0</a></li>
+					<li><a href="https://fontawesome.com/icons/user?style=solid">"user (Solid Style)"</a> by <a href="https://fontawesome.com/license/free">FontAwesome</a> is licenced under <a href="https://creativecommons.org/licenses/by/4.0/">CC BY 4.0</a></li>
+					<li><a href="https://loading.io/icon/kbmzeh">"comet" spinner</a> by <a href="https://loading.io/">loading.io</a>  is licenced under <a href="https://creativecommons.org/licenses/by/4.0/">CC BY 4.0</a></li>
 				</ul>
 			</div>
 		</Layout>
