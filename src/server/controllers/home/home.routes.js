a1927a8d1a8d63ed27b21649b0b4919b99044a95
MatthewWid <115mattey@gmail.com>
2019-08-18 13:56:40 +1000

    Wrap 'home.getStats' in home routing in generic middleware promise error handler.

--- src/server/controllers/home/home.routes.js
+++ src/server/controllers/home/home.routes.js
@@ -1,9 +1,15 @@
 const router = require("express").Router();
+const wrap = require("../../helpers/wrapAsync");
+
 const home = require("./home.controllers.js");
 
 // Debug API test response
-router.get("/ping", home.ping);
+router.get("/ping",
+	home.ping
+);
 // Get site statistics
-router.get("/stats", home.getStats);
+router.get("/stats",
+	wrap(home.getStats)
+);
 
 module.exports = router;
