f1575cb1c3c0c1b543e2e5f8ab4bdd3711cd6237
MatthewWid <115mattey@gmail.com>
2019-08-26 13:26:44 +1000

    Remove 'TODO:' code comments.

--- src/server/models/Post.js
+++ src/server/models/Post.js
@@ -48,34 +48,6 @@ const PostSchema = new mongoose.Schema({
 });
 
 // Virtuals
-/*
-	TODO:
-		Mongoose/MongoDB cannot sort by virtual fields yielding it impossible
-		to retrieve posts in the order of most liked or even applying some
-		special algorithm to the returned results whilst simulatenously limiting
-		the amount of results to eb retrieved.
-
-		A workaround must be used instead.
-		Either:
-
-		- Use an aggregate $lookup in each controller that retrieves any post.
-
-			Not DRY unless	a new helper function is created
-
-		- Store a hard (not virtual) field that stores the total likes the post has accumulated
-		and whenever the `addLike` controller is triggered it increments it, and whenever the
-		`removeLike` controller is triggered it decrements it.
-
-			Could possibly make data out of sync - a Like must be added and THEN the like counter
-			must be increased. The 'totalLikes' field would be independent from the *actual* amount
-			of likes and would only be assumed to be correct.
-
-		- Use a sub-document array of user IDs that have liked the post instead of having a
-		separate model for 'Like's and count the length of the array.
-		
-			Documents have a hard 16MB limit and will use a lot more memory when an array of
-			many posts with many posts is loaded.
-*/
 PostSchema.virtual("totalLikes", {
 	ref: "Like",
 	localField: "_id",
